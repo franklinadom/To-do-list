2fb18e3f901ff12b54b24f8af79e0de331d01439
Adom Franklin <franlinadom03@gmail.com>
2026-08-06 00:31:22 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import { useState } from 'react'
+
 import './App.css'
+import { FaTrash } from "react-icons/fa";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className='container1'>
-      <p className='text'>Enter task</p>
+    <>
+    <div className='container'>
+      <div className='sub-container1'>
+        <p className='text'>Enter task</p>
+      </div>
+      <div className='sub-container2'>
+        <p className='text2'>Add</p>
+      </div>
     </div>
+
+    <div className='body'>
+      <div className='container2'>
+        <p className='text3'>Learn React</p>
+      </div>
+      <div className='container2'>
+        <p className='text3'>Build my first react proj</p>
+        <button className='delete-btn'><FaTrash/></button>
+      </div>
+    </div>
+    </>
   )
 }
 
